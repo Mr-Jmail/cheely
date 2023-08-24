173bc1fdd3df7df67eb85cdf7fc7ab1d05a018fc
Andrew <root@ovz1.andrey12233393.mzlgn.vps.myjino.ru>
2023-08-24 09:52:59 +0300

    animate

--- bd_config.js
+++ bd_config.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 module.exports = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "cheleebot",
+    password: "12233393777",
     database: "chelee",
     connectionLimit : 7,
 })
